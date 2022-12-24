3f9b3f33fab0ebd04e6ace08bfe34951c41f4271
Vishal <vishalkale151071@gmail.com>
2022-12-24 21:16:28 +0530

    added new script for installing docker and starting the ipfs node

--- docker/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker-compose up -d
-sleep 60
-docker-compose exec ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-docker-compose exec ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
