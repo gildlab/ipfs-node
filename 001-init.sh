647af8d5697cca3aabbd088fb398f7c6fe910756
vishal <vishalkale151071@gmail.com>
2022-12-29 14:55:42 +0000

    added 001-init.sh for cors and ReceiptMetadata.json

--- 001-init.sh
+++ 001-init.sh
@@ -1,2 +1,4 @@
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
+
+ipfs add --pin --to-files /  /export/ReceiptMetadata.json
--- setup.sh
+++ setup.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-
-IFS=$(printf '\n\t')
-
-# Docker
-sudo apt remove --yes docker docker-engine docker.io containerd runc || true
-sudo apt update
-sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
-wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
-sudo apt update
-sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
-sudo usermod --append --groups docker "$USER"
-sudo systemctl enable docker
-printf '\nDocker installed successfully\n\n'
-
-printf 'Waiting for Docker to start...\n\n'
-sleep 5
-
-# Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-printf '\nDocker Compose installed successfully\n\n'
-printf $(docker-compose --version)
-
 if [[ -f ".env" ]]
 then
     printf "\n\nexporting .env variables.\n\n"
@@ -66,11 +40,8 @@ fi
 source .env
 
 # Start Docker
+printf "\n\nStarting Docker."
 docker-compose up -d
-sleep 30
-
-# add ReceiptMetadata.json to ipfs
-curl -F file=@ReceiptMetadata.json 'http://127.0.0.1:5001/api/v0/add?pin=true&to-files=/'
 
 # opening firewall ports for communication
 sudo ufw enable
