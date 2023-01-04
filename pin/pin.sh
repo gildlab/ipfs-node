f4056b8bb3c9b6b6b8c14089a9e117be628c21ef
thedavidmeister <thedavidmeister@gmail.com>
2023-01-04 22:19:18 +0400

    debug healthcheck

--- pin/pin.sh
+++ pin/pin.sh
@@ -4,40 +4,80 @@
 set -Eeuxo pipefail
 
 # get the hashes from the subgraph
-
-url='https://api.thegraph.com/subgraphs/name/gild-lab/offchainassetvault'
-id='0x8058ad7c22fdc8788fe4cb1dac15d6e976127324';
-query=$( cat << "QUERY"
-    query($id: String!) {
-        hashes(first:100, skip: 0, orderBy:timestamp, orderDirection: desc) {
-            hash
+get_hashes () {
+    local url='https://api.thegraph.com/subgraphs/name/gild-lab/offchainassetvault'
+    local id='0x8058ad7c22fdc8788fe4cb1dac15d6e976127324';
+    local query=$( cat << "QUERY"
+        query($id: String!) {
+            hashes(first:100, skip: 0, orderBy:timestamp, orderDirection: desc) {
+                hash
+            }
         }
-    }
 QUERY
-);
-payload=$( cat << "PAYLOAD"
-    {
-        query: $query,
-        variables: {
-            id: $id
+    );
+    local payload=$( cat << "PAYLOAD"
+        {
+            query: $query,
+            variables: {
+                id: $id
+            }
         }
-    }
 PAYLOAD
-);
-jq_selector='.data.hashes[].hash | select(startswith("Qm"))';
+    );
+    local jq_selector='.data.hashes[].hash | select(startswith("Qm"))';
 
-body=$( jq -n --arg query "$query" --arg id $id "$payload" );
-hashes=$( curl -X POST -d "$body" $url | jq -r "$jq_selector" );
+    local body=$( jq -n --arg query "$query" --arg id $id "$payload" );
+
+    curl -X POST -d "$body" $url | jq -r "$jq_selector"
+}
+
+# determine if kubo is available direct
+is_ipfs_running() {
+    ipfs id &>/dev/null
+}
+
+# pin using kubo directly
+pin_direct() {
+    echo "$0" | xe -j10x ipfs pin add
+}
+
+# detect the host of the service
+service_host() {
+    if [ -z $( dig +short gl_ipfs ) ]
+        then
+            echo "localhost"
+        else
+            echo "gl_ipfs"
+    fi
+}
+
+# pin using a service
+pin_service() {
+    local url="http://$( service_host ):5001/api/v0/pin/add?arg=";
+    sed 's#^#'"$url"'#g' <<< "$1" | xe -j10x curl -s -X POST
+}
 
 # pin the hashes to the ipfs node
+pin_hashes() {
+    local hashes="$( get_hashes )"
+    if is_ipfs_running
+        then 
+            pin_direct "$hashes"
+        else 
+            pin_service "$hashes"
+    fi
+}
 
-# detect if we are the machine running ipfs kubo itself
-if command -v ipfs &> /dev/null
-    then
-        echo "$hashes" | xe -j10x ipfs pin add {}
-    else
-        # detect if we're the docker host or guest to try to curl the machine that is running kubo
-        host=$( if [ -z $( dig +short gl_ipfs ) ]; then echo "localhost"; else echo "gl_ipfs"; fi )
-        url="http://$host:5001/api/v0/pin/add?arg="
-        sed 's#^#'"$url"'#g' <<< "$hashes" | xe -j10x curl -s -o /dev/null -X POST
+# main loop
+loop() {
+    while true
+        do
+            pin_hashes
+            sleep 10
+        done
+}
+
+if $1
+    then loop
+    else pin_hashes
 fi
