f2f135eb5fc36b36b3758a574d643014414bbee8
thedavidmeister <thedavidmeister@gmail.com>
2023-01-04 15:41:36 +0400

    debug ipfs

--- ipfs/container-init.d/003-announce-ngrok.sh
+++ ipfs/container-init.d/003-announce-ngrok.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -euxo pipefail
 
-ipfs config --json Addresses.AppendAnnounce '["/dns4/'"$NGROK_EDGE_HOSTNAME"'/tcp/'"$NGROK_EDGE_PORT"'"]'
+ipfs config --json Addresses.AppendAnnounce '["/dns/'"$NGROK_EDGE_HOSTNAME"'/tcp/'"$NGROK_EDGE_PORT"'"]'
