fc29a51b2ae8a61e3ca4c505cb6a5d6f93f1f0c9
thedavidmeister <thedavidmeister@gmail.com>
2023-01-31 17:04:53 +0400

    cors on nginx

--- ipfs/container-init.d/001-set-allow-origin.sh
+++ ipfs/container-init.d/001-set-allow-origin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -euxo pipefail
 
-#https://github.com/ipfs-shipyard/go-ipfs-docker-examples/blob/main/gateway/ipfs-config.sh
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+# https://github.com/ipfs-shipyard/go-ipfs-docker-examples/blob/main/gateway/ipfs-config.sh
+# ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["POST"]'
