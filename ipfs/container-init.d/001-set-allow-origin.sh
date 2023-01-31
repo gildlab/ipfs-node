b9da9b29f5c4d9f6077ea03ba1193f2b610d4f04
thedavidmeister <thedavidmeister@gmail.com>
2023-01-31 18:54:37 +0400

    debug

--- ipfs/container-init.d/001-set-allow-origin.sh
+++ ipfs/container-init.d/001-set-allow-origin.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 
 # https://github.com/ipfs-shipyard/go-ipfs-docker-examples/blob/main/gateway/ipfs-config.sh
-# ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["POST"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["POST, OPTIONS"]'
