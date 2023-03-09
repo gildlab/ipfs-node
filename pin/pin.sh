467f9140dd4371987da9deb7ce59a97f0e5556e4
thedavidmeister <thedavidmeister@gmail.com>
2023-03-09 20:14:58 +0400

    more deployers

--- pin/pin.sh
+++ pin/pin.sh
@@ -7,7 +7,7 @@ set -Eeux
 
 # query for hashes from the subgraph
 build_query () {
-    local ids=('0x8058ad7c22fdc8788fe4cb1dac15d6e976127324' '0xc0D477556c25C9d67E1f57245C7453DA776B51cf' '0x6E37d34e35a5fF2f896eD9e76EC43e728adA1d18');
+    local ids=('0x8058ad7c22fdc8788fe4cb1dac15d6e976127324' '0xc0D477556c25C9d67E1f57245C7453DA776B51cf' '0x6E37d34e35a5fF2f896eD9e76EC43e728adA1d18' '0x2cb21fb0a2cebb57434b1a2b89c81e5f49cd484a' '0xaa1decefc2b32ca6390c9773e4ecffe69a644ff7' '0x627a12ce1f6d42c9305e03e83fe044e8c3c1a32c');
     # Build a lowercased json array from ids bash array
     local jsonids=$( jq -c -n '$ARGS.positional' --args "${ids[@],,}" )
     local query=$( cat << "QUERY"
