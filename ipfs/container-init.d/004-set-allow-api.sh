7927daa69fcb835c9f54d43071339afc2c0da680
thedavidmeister <thedavidmeister@gmail.com>
2023-06-27 19:53:48 +0400

    fix json

--- ipfs/container-init.d/004-set-allow-api.sh
+++ ipfs/container-init.d/004-set-allow-api.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 # This is inside the docker container, nginx wraps it.
-ipfs config --json Addresses.API "/ip4/0.0.0.0/tcp/5001"
+ipfs config --json Addresses.API '"/ip4/0.0.0.0/tcp/5001"'
